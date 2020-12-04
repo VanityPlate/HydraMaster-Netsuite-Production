0478155af5315d9a0fcfa128f799965c9978132f
VanityPlate <alexducken@gmail.com>
2020-12-04 12:02:51 -0800

    testing date formating

--- FileCabinet/SuiteScripts/Warranty System/Manual Warranty SS.js
+++ FileCabinet/SuiteScripts/Warranty System/Manual Warranty SS.js
@@ -135,7 +135,7 @@ function(record, search, runtime, fieldLib, format) {
             });
             warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_item', value: invoiceSearchObj[0].getValue({name: 'item'})});
             //Refactor Testing
-            var dateCreated = format.parse({value: invoiceSearchObj[0].getValue({name: 'datecreated'}), type: format.Type.MMYYDATE});
+            var dateCreated = format.parse({value: invoiceSearchObj[0].getValue({name: 'datecreated'}), type: format.Type.DATETIMETZ});
             log.audit({title: 'Date Created', details: dateCreated});
             warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_invoicedate', value: dateCreated});
             warrantyObj.setValue({fieldId: 'custrecord_invoice_reference', value: invoiceSearchObj[0].getValue({name: 'internalid'})});
