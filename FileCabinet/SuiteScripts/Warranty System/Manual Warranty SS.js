e45fc0d62e1472946d0b8869b9d93c466d2f997f
VanityPlate <alexducken@gmail.com>
2020-12-04 12:51:33 -0800

    testing date formating

--- FileCabinet/SuiteScripts/Warranty System/Manual Warranty SS.js
+++ FileCabinet/SuiteScripts/Warranty System/Manual Warranty SS.js
@@ -136,15 +136,14 @@ function(record, search, runtime, fieldLib, format) {
             warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_item', value: invoiceSearchObj[0].getValue({name: 'item'})});
             //Refactor Testing
             var dateCreated = format.parse({value: invoiceSearchObj[0].getValue({name: 'datecreated'}), type: format.Type.DATE});
-            log.audit({title: 'Date Created', details: dateCreated});
             warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_invoicedate', value: dateCreated, ignoreFieldChange: true});
             warrantyObj.setValue({fieldId: 'custrecord_invoice_reference', value: invoiceSearchObj[0].getValue({name: 'internalid'})});
             warrantyObj.setValue({fieldId: 'custrecord_selling_distributor', value: invoiceSearchObj[0].getValue({name: 'internalid', join: 'customerMain'})});
             warrantyObj.setValue({fieldId: 'custrecord_vehicle_vin', value: formZero[fieldLib.customerFields.vehicleVIN.id]});
             //Setting Warranty Date
-            var startDate = formZero[fieldLib.entrySelect.installDate.id];
+            var startDate = format.parse({value: formZero[fieldLib.entrySelect.installDate.id], type: format.Type.DATE});
             var terms = warrantyObj.getValue({fieldId: 'custrecord_wrm_reg_warrantyterm'});
-            warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_warrantybegin', value: startDate});
+            warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_warrantybegin', value: startDate, ignoreFieldChange: true});
             warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_warrantyterm', value: null});
             warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_warrantyterm', value: terms});
 
