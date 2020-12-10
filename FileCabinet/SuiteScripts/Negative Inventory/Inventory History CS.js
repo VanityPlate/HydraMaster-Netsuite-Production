cea0e50bc9827357c3153b35bc43df965146c30c
VanityPlate <alexducken@gmail.com>
2020-12-10 11:00:11 -0800

    change to item history

--- FileCabinet/SuiteScripts/Negative Inventory/Inventory History CS.js
+++ FileCabinet/SuiteScripts/Negative Inventory/Inventory History CS.js
@@ -32,7 +32,7 @@ function(currentRecord, getInternal, message, search) {
             myPage.data.forEach(function(result){
                 var type = result.getValue({name: 'type', summary: 'GROUP'});
                 var quantity =  parseInt(result.getValue({name: 'quantity', summary: 'MIN'}));
-                if(type == 'BinWksth' || type == 'WorkOrd' || type == 'PurchOrd' || type == 'WOIssue' || type == 'WOClose' || type == 'TrnfrOrd' || type == 'CustCred' || type == 'BinTrnfr'){}
+                if(type == 'BinWksht' || type == 'WorkOrd' || type == 'PurchOrd' || type == 'WOIssue' || type == 'WOClose' || type == 'TrnfrOrd' || type == 'CustCred' || type == 'BinTrnfr'){}
                 else {
                     //Calculating Totals
                     switch(type){
