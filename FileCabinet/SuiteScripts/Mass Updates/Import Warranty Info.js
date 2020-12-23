3d32ca1bf1a8ba2461ebfcff14f785a4814f6aa0
VanityPlate <alexducken@gmail.com>
2020-12-22 16:58:49 -0800

    -mass update to add warranties underway

--- FileCabinet/SuiteScripts/Mass Updates/Import Warranty Info.js
+++ FileCabinet/SuiteScripts/Mass Updates/Import Warranty Info.js
@@ -5,12 +5,56 @@
  * @NApiVersion 2.1
  * @NScriptType MapReduceScript
  */
-define(['N/record', 'N/search', 'N/file'],
+define(['N/record', 'N/search', 'N/file', 'N/format'],
     /**
  * @param{record} record
  * @param{search} search
  */
-    (record, search, file) => {
+    (record, search, file, format) => {
+
+        /**
+         * Constants
+         */
+        const items = {
+            BOXXER_WPH: 23860,
+            CTS_WPH: 23862,
+            CDS_WPH: 23861,
+            GTXR_WPH: 23863,
+            TITAN_WPH: 23864
+        };
+
+        const CUSTOMER_WPH = 21188;
+
+        const PHONE_REGX = /( |\(|\)|-)/ig;
+
+        /**
+         * Creates an individual customer
+         * @param {values} information used to create customer
+         * @returns {integer} the new customer internal id
+         */
+        function createCustomer(values){
+            try{
+                //Creating Record
+                var customerObj = record.create({
+                   isDynamic: true,
+                   type: record.Type.CUSTOMER
+                });
+
+                //Setting Fields
+                customerObj.setValue({fieldId: 'isperson', value: true});
+                customerObj.setValue({fieldId: 'companyname', value: values[7]});
+                customerObj.setValue({fieldId: 'phone', value: values[14].replace(PHONE_REGX, '')});
+                customerObj.setValue({fieldId: 'email', value: values[13]});
+
+                //Saving and Returning Internal ID
+                return customerObj.save();
+            }
+            catch (error){
+                log.error({title: 'Critical error in createCustomer', details: error});
+            }
+        }
+
+
         /**
          * Defines the function that is executed at the beginning of the map/reduce process and generates the input data.
          * @param {Object} inputContext
@@ -52,7 +96,60 @@ define(['N/record', 'N/search', 'N/file'],
 
         const map = (mapContext) => {
             try{
-                log.audit({title: 'Testing Value', details: mapContext.value});
+                //Capturing line values
+                var values = mapContext.value.split(',');
+
+                //Creating Record
+                var warrantyObj = record.create({type: 'customrecord_wrm_warrantyreg', isDynamic: true});
+
+                //Determining Item
+
+
+                //Finding Distributor
+                var distributor = search.create({
+                    type: "customer",
+                    filters:
+                        [
+                            ["entityid","contains",values[5]]
+                        ],
+                    columns:
+                        [
+                            search.createColumn({name: "internalid", label: "Internal ID"})
+                        ]
+                }).run().getRange({start: 0, end: 1});
+                if(distributor.length > 0){
+                    distributor = distributor[0].id;
+                }
+                else{
+                    distributor = CUSTOMER_WPH;
+                }
+
+                //Finding End User
+                var customer = search.create({
+                    type: "customer",
+                    filters:
+                        [
+                            ["phone","haskeywords",values[14].replace(PHONE_REGX, '')]
+                        ],
+                    columns:
+                        [
+                            search.createColumn({name: "internalid", label: "Internal ID"})
+                        ]
+                }).run().getRange({start: 0, end: 1});
+                if(customer.length > 0){
+                    customer = customer[0].id;
+                }
+                else{
+                    customer = createCustomer(values);
+                }
+
+
+                //Setting Record Values
+                warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_ref_seriallot', value: values[1]});
+                warrantyObj.setValue({fieldId: 'custrecord_wrm_reg_customer', value: distributor});
+                warrantyObj.setValue({fieldId: 'custrecord_selling_distributor', value: customer});
+
+                //Saving Record
             }
             catch(error){
                 log.error({title: 'Critical error in map', details: error});
