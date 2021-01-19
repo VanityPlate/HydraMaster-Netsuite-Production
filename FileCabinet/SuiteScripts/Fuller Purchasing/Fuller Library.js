6a2b11bceff8200d3020fd27910be6c1e5877d2c
VanityPlate <alexducken@gmail.com>
2021-01-19 12:41:12 -0800

    -correction for shipmethod to fuller automation

--- FileCabinet/SuiteScripts/Fuller Purchasing/Fuller Library.js
+++ FileCabinet/SuiteScripts/Fuller Purchasing/Fuller Library.js
@@ -10,7 +10,7 @@ function() {
      * Enum holding relation between PO and SO ship method.
      */
     const POSO = {
-        4:      {carrier: 'ups', method: 23313},
+        4:     {carrier: 'ups', method: 23313},
         23870: {carrier: 'nonups', method: 23312},
         20224: {carrier: 'nonups', method: 23312}
     };
