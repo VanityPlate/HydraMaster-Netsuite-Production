154547d3d236f7e878d4085ca3d9e881de7edc6d
VanityPlate <alexducken@gmail.com>
2021-02-08 14:27:56 -0800

    -updating for dates on kansas items

---
+++ FileCabinet/SuiteScripts/Fuller Purchasing/Sale Order Ship Date.js
@@ -0,0 +1,186 @@
+/**
+ *
+ * @copyright Alex S. Ducken 2021 alexducken@gmail.com
+ * @copyright HydraMaster LLC
+ *
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['SuiteScripts/Fuller Purchasing/Ship Dates Lib.js', 'SuiteScripts/Help_Scripts/Moment.js'],
+
+function(shipDatesLib, moment) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+        try{
+            debugger;
+            if(scriptContext.sublistId === 'item'){
+                if(scriptContext.currentRecord.getCurrentSublistValue({fieldId: 'location', sublistId: 'item'}) == shipDatesLib.LOCATION){
+                    var itemId = scriptContext.currentRecord.getCurrentSublistValue({sublistId: 'item', fieldId: 'item'});
+                    var date = moment.add(1, 'days');
+                    //Refactor Testing
+                    log.audit({title: 'Checking Date', details: date});
+                }
+            }
+            return true;
+        }
+        catch(error){
+
+        }
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+    }
+
+    return {
+        //pageInit: pageInit,
+        //fieldChanged: fieldChanged,
+        //postSourcing: postSourcing,
+        //sublistChanged: sublistChanged,
+        //lineInit: lineInit,
+        //validateField: validateField,
+        validateLine: validateLine,
+        //validateInsert: validateInsert,
+        //validateDelete: validateDelete,
+        //saveRecord: saveRecord
+    };
+    
+});
---
+++ FileCabinet/SuiteScripts/Fuller Purchasing/Ship Dates Lib.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @copyright Alex S. Ducken 2021 alexducken@gmail.com
+ * @copyright HydraMaster LLC
+ *
+ * @NApiVersion 2.x
+ */
+define([],
+    
+    function(){
+
+        const LOCATION = 16;
+        const DEFAULT = 10;
+        const ITEM_DATES = {};
+
+        return {
+                DEFAULT: DEFAULT,
+                ITEM_DATES: ITEM_DATES,
+                LOCATION: LOCATION
+        }
+
+    });
